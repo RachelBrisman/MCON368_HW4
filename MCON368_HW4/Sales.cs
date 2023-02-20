7f14a32e34848f5bb44ec7791c5e12a642853afa
RachelBrisman <rachaelibrisman@gmail.com>
2023-02-20 17:26:08 +0200

    added class called sales

---
+++ MCON368_HW4/Sales.cs
@@ -0,0 +1,13 @@
+namespace MCON368_HW4;
+
+public class Sales
+{
+    private Sale[] salesArray { get; set; }
+
+    public Sales(Sale[] sales)
+    {
+        salesArray = sales;
+    }
+    
+    
+}
